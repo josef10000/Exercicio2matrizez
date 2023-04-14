b3e39ef150fc7b20206a297b9326543fe2c58729
José Frazão <102549534+josef10000@users.noreply.github.com>
2023-04-14 16:55:40 -0300

    Add files via upload

---
+++ matrizes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[,] matriz = new int[3, 3];
+
+        Console.WriteLine("Por favor, preencha a matriz 3x3 com valores numéricos: ");
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                Console.Write($"Elemento [{i + 1},{j + 1}]: ");
+                matriz[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        for (int i = 0; i < 3; i++)
+        {
+            int somaLinha = 0;
+            for (int j = 0; j < 3; j++)
+            {
+                somaLinha += matriz[i, j];
+            }
+            Console.WriteLine($"Soma da Linha {i + 1}: {somaLinha}");
+        }
+
+        // Calcula a soma dos elementos de cada coluna
+        for (int j = 0; j < 3; j++)
+        {
+            int somaColuna = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                somaColuna += matriz[i, j];
+            }
+            Console.WriteLine($"Soma da Coluna {j + 1}: {somaColuna}");
+        }
+    }
+}
